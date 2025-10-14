8b0a8da109576b692070e46c8833f4a158d6e9b0
cyoure <wwj9559@gmail.com>
2025-10-14 23:22:07 +0900

    이벤트 처리

--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -1,9 +1,33 @@
 export default function Hello() {
+  function showName() {
+    console.log("Mike");
+  }
+
+  function showAge(age) {
+    console.log(age);
+  }
+
+  function showText(e) {
+    console.log(e);
+  }
   return (
     <div>
       <h1>Hello</h1>
-      <button>Show name</button>
-      <button>Show age</button>
+      <button onClick={showName}>Show name</button>
+      <button
+        onClick={() => {
+          showAge(10);
+        }}
+      >
+        Show age
+      </button>
+      <input
+        type="text"
+        onChange={(e) => {
+          const text = e.target.value;
+          showText(text);
+        }}
+      />
     </div>
   );
 }
