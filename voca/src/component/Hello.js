69a172bd2b7fa65b6f7372cb800029c6302c1741
cyoure <wwj9559@gmail.com>
2025-10-14 18:59:34 +0900

    test

--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -1,19 +1,9 @@
-import styles from "./Hello.module.css";
-
 export default function Hello() {
   return (
     <div>
-      <h1
-        style={{
-          color: "#f00",
-          borderRight: "2px solid #000",
-          marginBottom: "50px",
-          opacity: 1,
-        }}
-      >
-        Hello
-      </h1>
-      <div className={styles.box}> Hello</div>
+      <h1>Hello</h1>
+      <button>Show name</button>
+      <button>Show age</button>
     </div>
   );
 }
