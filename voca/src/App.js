56dbdb22fe900a5c6c8f62a364ece721339447d1
cyoure <wwj9559@gmail.com>
2025-10-17 01:47:37 +0900

    라우터 구현 react-router-dom

--- voca/src/App.js
+++ voca/src/App.js
@@ -1,14 +1,21 @@
 import DayList from "./component/DayList";
 import Header from "./component/Header";
 import Day from "./component/Day";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmptyPage from "./component/EmptyPage";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <DayList />
-      <Day></Day>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<DayList />}></Route>
+          <Route path="/day/:day" element={<Day />}></Route>
+          <Route path="*" element={<EmptyPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- voca/src/component/Day.js
+++ voca/src/component/Day.js
@@ -1,13 +1,15 @@
 import dummy from "../db/data.json";
+import { useParams } from "react-router-dom";
 
 export default function Day() {
   // dummy.words
-  const day = 1;
-  const wordList = dummy.words.filter((word) => word.day === 1);
-  console.log(wordList);
+  const { day } = useParams();
+
+  const wordList = dummy.words.filter((word) => word.day === Number(day));
 
   return (
     <>
+      <h2> Day {day}</h2>
       <table>
         <tbody>
           {wordList.map((word) => (
