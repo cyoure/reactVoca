8205ed0fc7f3460051d7e3bbf1f60610f5cf869d
cyoure <wwj9559@gmail.com>
2025-10-18 01:39:38 +0900

    useEffect, fetch로 API호출

--- voca/src/component/DayList.js
+++ voca/src/component/DayList.js
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import dummy from "../db/data.json";
 
 export default function DayList() {
-  console.log(dummy);
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/days")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setDays(data);
+      });
+  }, []); // 렌더링 직후 한번만 호출하고 싶을 땐 빈 배열을 사용하면 된다.
 
   return (
     <ul className="list_day">
-      {dummy.days.map((day) => (
+      {days.map((day) => (
         <li key={day.id}>
           <Link to={`/day/${day.day}`}> Day {day.day}</Link>
         </li>
